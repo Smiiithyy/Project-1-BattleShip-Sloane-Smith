87349dc6e8fdcaeca659a408f64913f6162e66d7
ss18026 <sloane.smith95@gmail.com>
2022-05-06 10:53:44 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,131 +1,206 @@
-gridNumber = 5
-playerOneShips = 3
-enemyShips = 3
+gridNumber = prompt("How many rows/columns do you want your grid to be?")
+playerOneShips = 0;
+enemyShips = 3;
+let hitplayer = [];
+let hitenemy = [];
+
+alert("please place 3 ships on your player board")
+
+let playerTable = document.getElementById("playerBoard");
+let enemyTable = document.getElementById("enemyBoard");
+d=1
+for (a = 0; a < gridNumber; a++) {
+  let row = playerTable.insertRow();
+  for (b = 0; b < gridNumber; b++) {
+    let cell = row.insertCell(b);
+    cell.classList.add("playerBoardCell");
+    cell.innerHTML = " ";
+    cell.setAttribute("num", d++)
+  }
+}
+let c = 1
+for (a = 0; a < gridNumber; a++) { 
+  let row = enemyTable.insertRow();
+  for (b = 0; b < gridNumber; b++) {
+    let cell = row.insertCell(b);
+    cell.classList.add("enemyBoardCell");
+    cell.innerHTML = "";
+    cell.setAttribute("id", c++)
+        }
+  }
 
 
-function BoardSize(size) {
-    let grid = [];
-    for(i=0; i < size; i++) {
-        grid[i] = [];
-    for(j=0; j < size; j++) {
-        grid[i][j] = ' ';
-    }
-    }
-    return grid;
+
+
+
+let enemyCoordinate = document.querySelectorAll(".enemyBoardCell");
+let playerCoordinate = document.querySelectorAll(".playerBoardCell");
+
+// Placing Player Ships
+// if(playerOneShips<3) {
+  playerCoordinate.forEach((el) => {
+    el.addEventListener("click", (event) => {
+      if (el.innerHTML === " ") {
+        event.target.style.backgroundColor = "blue";
+        event.target.innerHTML = "x"
+   
+      }
+      playerOneShips++;
+      console.log(playerOneShips)
+    });
+
+  });
+// }
+
+//Placing Enemy Ships
+for(let i=0; i<enemyShips; i++){
+    shipPlacement = Math.round(Math.random() * 24)+1;
+    console.log(shipPlacement)
+    randomselector = document.querySelector(`[id = "${shipPlacement}"`)
+    // randomselector.innerHTML ='x'
+    console.log((`[id = "${shipPlacement}"`))
+    console.log(document.querySelector(`[id = "${shipPlacement}"`).innerHTML)
+
+    randomselector.setAttribute("ship","x")
 }
 
-let playerOneBoard = BoardSize(gridNumber)
-let enemyBoard = BoardSize(gridNumber)
-console.log(playerOneBoard)
-console.log(enemyBoard)
 
-for(i=1; i<=playerOneShips; i++) {
-    playerShipLocation = [];
-    enemyShipLocation = [];
-    columnNumberP = prompt(`What column do you want you ship ${i} to be in?`)
-    rowNumberP = prompt(`What row do you want ship ${i} to be in?`)
-    let shipLocation = placeShips(columnNumberP, rowNumberP, 'x', playerOneBoard)
-    playerShipLocation = playerShipLocation.push(shipLocation)
-
-
-    // Will be there be an error if they place the ship in the same place
-    columnNumberE = Math.round((Math.random()*3))
-    rowNumberE = Math.round((Math.random()*3))
-    placeEnemyShips (columnNumberE, rowNumberE, 'x', enemyBoard)
-    console.log(columnNumberE,rowNumberE)
-}
-// console.log(playerShipLocation)
-
-
-function placeShips(columnNumberP, rowNumberP, position, grid) {
-    grid[columnNumberP][rowNumberP] = position;
-}
-
-
-function placeEnemyShips(columnNumberE, rowNumberE, position, grid) {
-    grid[columnNumberE][rowNumberE] = position;
-}
-
-//While Loop - need this to continue until all enemy ships are hit 
-// function playerAttack(column,row, attackPosition, enemyBoard){
-
-for(y=0; y<3; y++){
-   column = prompt('Select a column for your attack')
-    row = prompt("Select a row for your attack")
-    // if(grid[columnE][rowE] == 'x'){
-    placePlayerAttack(column, row, enemyBoard)
-    // } else if(grid[columnE][rowE] = ' ') {
-    //     placePlayerAttack(column,row, 'o', enemyBoard)
-    // }
-    columnE = Math.round((Math.random()*3))
-    rowE = Math.round((Math.random()*3))
-
-    placeEnemyAttack(columnE,rowE, playerOneBoard)
-
-    console.log(playerOneBoard)
-    console.log(enemyBoard)
-}
-
-
-    // placeEnemyAttack(columnE,rowE,'o', playerOneBoard)
-
-
-
-function placePlayerAttack(column, row, grid){ 
-if ((grid[column][row]) == 'x') {
-    (grid[column][row]) = '!';
-    return true
-} else if (grid[column][row] == ' ') {
-    (grid[column][row]) = 'o'
-    return false
-} else {
-    return false
-}
-}
-    
-
-
-
-
-function placeEnemyAttack(columnE, rowE, grid){ 
-    if ((grid[columnE][rowE]) == 'x') {
-        (grid[columnE][rowE]) = '!';
-        return true
-    } else if (grid[columnE][rowE] == ' ') {
-        (grid[columnE][rowE]) = 'o'
-        return false
+//Determining if we hit the enemy
+enemyCoordinate.forEach((el) => {
+  el.addEventListener("click", (event) => {
+      playerAction = document.querySelector(".betweenBoards")
+      console.log(el.getAttribute("ship"))
+    if (el.getAttribute("ship") === "x") {
+      event.target.style.backgroundColor = "red";
+      enemyShips--
+      playerAction.innerHTML = "ENEMY HAS BEEN HIT!! Please attack again!"
+      console.log(enemyShips)
     } else {
-        return false
+      event.target.style.backgroundColor = "grey";
+      console.log(el.innerHTML);
+      playerAction.innerHTML = "You Missed. Please attack again"
     }
-    
 
+    enemyAttackSelection = Math.round(Math.random() * 24); 
+    //Need to figure out the double selection
+ 
+    randomFire = document.querySelector(`[num = "${enemyAttackSelection}"]`)
+    // console.log(randomFire.innerHTML)
+    if(randomFire.style.backgroundColor == "blue"){
+    randomFire.style.backgroundColor = "red";
+    } else {
+    randomFire.style.backgroundColor = "grey"
+    }
+  });
+});
+
+//Determining if the Enemy has hit us 
+
+
+if(enemyShips === 0){
+    alert("Player has won! Congrats!")
+}else if(playerOneShips === 0){
+    alert("Your Ships have been sunk. You lose")
 }
 
+//
 
 
 
 
+// let playerOneBoard = BoardSize(gridNumber);
+// // let enemyBoard = BoardSize(gridNumber);
+// console.log(playerOneBoard);
+// console.log(enemyBoard);
+
+// for (let i = 1; i <= playerOneShips; i++) {
+//   playerShipLocation = [];
+//   enemyShipLocation = [];
+
+//   columnNumberP = prompt(`What column do you want your ship ${i} to be in?`, i);
+//   rowNumberP = prompt(`What row do you want ship ${i} to be in?`, i);
+//   placeShips(columnNumberP, rowNumberP, "x", playerOneBoard);
 
 
 
+//   // Will be there be an error if they place the ship in the same place
+//   columnNumberE = Math.round(Math.random() * 3);
+//   rowNumberE = Math.round(Math.random() * 3);
+//   enemyShipLocation = [columnNumberE,rowNumberE]
 
 
 
-
-
-
-// function placeEnemyAttack(columnE, rowE, enemyAttackPosition, grid) {
-//     columnE = Math.round((Math.random()*3))
-//     rowE = Math.round((Math.random()*3))
-//     grid[columnE][rowE] = enemyAttackPosition
-//     if(enemyAttackPosition == 'x'){
-//         return enemyAttackPosition == '!'
-//     } else {
-//        return enemyAttackPosition == 'o'
-//     }
+//   console.log(enemyShipLocation)
+//   placeEnemyShips(columnNumberE, rowNumberE, "x", enemyTable);
 
 // }
 
 
-console.log(playerOneBoard)
-console.log(enemyBoard)
+// function placeShips(columnNumberP, rowNumberP, position, grid) {
+//   // for(z=0; z<playerOneShips; z++){
+//   grid[columnNumberP][rowNumberP] = position;
+// }
+
+
+
+// function placeEnemyShips(columnNumberE, rowNumberE, position, grid) {
+//   grid[columnNumberE][rowNumberE] = position;
+// }
+
+
+
+// while (enemyShips !== 0 || playerOneShips !== 0) {
+//   column = prompt("Select a column for your attack");
+//   row = prompt("Select a row for your attack");
+
+//   placePlayerAttack(column, row, enemyBoard);
+
+//   if (hitenemy == true) {
+//     enemyShips--;
+//   }
+//   columnE = Math.round(Math.random() * 3);
+//   rowE = Math.round(Math.random() * 3);
+
+//   placeEnemyAttack(columnE, rowE, playerOneBoard);
+//   if (hitplayer == true) {
+//     playerOneShips--;
+//   }
+//   if (enemyShips == 0) {
+//     console.log("Player 1 WON");
+//     break;
+//   } else if (playerOneShips == 0) {
+//     console.log("Enemy Won!");
+//     break;
+//   }
+//   console.log(enemyShips);
+//   console.log(playerOneShips);
+
+//   console.log(playerOneBoard);
+//   console.log(enemyBoard);
+// }
+
+// placeEnemyAttack(columnE,rowE,'o', playerOneBoard)
+
+// function placePlayerAttack(column, row, grid) {
+//   if (grid[column][row] == "x") {
+//     grid[column][row] = "!";
+//     return (hitenemy = true);
+//   } else if (grid[column][row] == " ") {
+//     grid[column][row] = "o";
+//     return (hitenemy = false);
+//   } else {
+//     return (hitenemy = false);
+//   }
+// }
+
+// function placeEnemyAttack(columnE, rowE, grid) {
+//   if (grid[columnE][rowE] == "x") {
+//     grid[columnE][rowE] = "!";
+//     return (hitplayer = true);
+//   } else if (grid[columnE][rowE] == " ") {
+//     grid[columnE][rowE] = "o";
+//     return (hitplayer = false);
+//   } else {
+//     return (hitplayer = false);
+//   }
+// }
